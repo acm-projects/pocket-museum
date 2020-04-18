6fcf96275fc400999adb7fbaee39324996628fc5
Soham Mukherjee <sohammukherjee2000@gmail.com>
2020-04-17 19:41:18 -0500

    Update homepage.dart

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -69,8 +69,7 @@ class _HPState extends State<Home> {
 
   Future uploadImage() async {
     // upload the image to firebase storage
-    String fileID = DateTime.now().toString();
-    myImageName = fileID.toString();
+    myImageName = _imageFile.toString() + DateTime.now().toString();
     _reference = FirebaseStorage.instance.ref().child(myImageName);
     StorageUploadTask uploadTask = _reference.putFile(_imageFile);
     StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
@@ -120,17 +119,87 @@ class _HPState extends State<Home> {
           title: Text("Pocket Museum", style: TextStyle(fontFamily: fam)),
           backgroundColor: Colors.orange[800],
         ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              //Font Settings
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text("Settings",
+                    style: TextStyle(
+                        fontFamily: fam,
+                        fontSize: 30,
+                        decoration: TextDecoration.underline)),
+              ),
+
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
+                  Widget>[
+                Text("Font: ", style: TextStyle(fontFamily: fam, fontSize: 20)),
+                ToggleButtons(
+                  children: <Widget>[
+                    Text("A",
+                        style:
+                            TextStyle(fontFamily: 'Comic Neue', fontSize: 10)),
+                    Text("A",
+                        style:
+                            TextStyle(fontFamily: 'Montserrat', fontSize: 10)),
+                    Text("A",
+                        style: TextStyle(fontFamily: 'OpenSans', fontSize: 10)),
+                  ],
+                  onPressed: (int index) {
+                    setState(() {
+                      for (int buttonIndex = 0;
+                          buttonIndex < isSelected.length;
+                          buttonIndex++) {
+                        if (buttonIndex == index) {
+                          isSelected[buttonIndex] = true;
+                          fam = fonts[buttonIndex];
+                        } else {
+                          isSelected[buttonIndex] = false;
+                        }
+                      }
+                    });
+                  },
+                  isSelected: isSelected,
+                ),
+              ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Toggle Mode: ",
+                    style: TextStyle(fontFamily: fam, fontSize: 20),
+                  ),
+                  Switch(
+                    value: (_themeChanger.getTheme() == ThemeData.dark()),
+                    onChanged: (value) {
+                      setState(() {
+                        _themeChanger.getTheme() == ThemeData.dark()
+                            ? _themeChanger.setTheme(ThemeData.light())
+                            : _themeChanger.setTheme(ThemeData.dark());
+                      });
+                    },
+                    activeTrackColor: Colors.orange[500],
+                    activeColor: Colors.orange[800],
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(30),
+                child: Image.asset(
+                  'assets/images/TranspMuseum.png',
+                  width: 100,
+                  height: 100,
+                  scale: 5,
+                ),
+              ),
+            ],
+          ),
+        ),
         body: SingleChildScrollView(
           child: Center(
               child: Column(children: <Widget>[
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Modify Artwork",
-                  style: TextStyle(
-                      fontFamily: fam,
-                      fontSize: 30,
-                      decoration: TextDecoration.underline)),
-            ),
+            Padding(padding: EdgeInsets.all(10)),
 // if there is no image selected, return an empty container
             // but if there is an image then display it.
             _imageFile == null
@@ -140,13 +209,18 @@ class _HPState extends State<Home> {
                     height: 300.0,
                     width: 300.0,
                   ),
+            Padding(padding: EdgeInsets.all(5)),
 // if the image is not selected don't show the option to upload to firebase storage
             _imageFile == null
                 ? Container()
-                : RaisedButton(
-                    color: Colors.orange[800],
-                    child: Text("Check for Info",
-                        style: TextStyle(fontFamily: fam)),
+                : FlatButton(
+                    color: Colors.transparent,
+                    child: Image.asset(
+                      'assets/images/black_button.png',
+                      width: 100,
+                      height: 100,
+                      scale: 5,
+                    ),
                     onPressed: () {
                       Alert(
                         context: context,
@@ -167,7 +241,8 @@ class _HPState extends State<Home> {
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => InfoPage(font:fam)));
+                                      builder: (context) =>
+                                          InfoPage(font: fam)));
                             },
                             width: 120,
                             radius: BorderRadius.circular(0.0),
@@ -178,25 +253,12 @@ class _HPState extends State<Home> {
                       uploadImage();
                     },
                   ),
-            // if the image is not selected don't show the option to upload to firebase storage
-            _imageFile == null
-                ? Container()
-                : RaisedButton(
-                    color: Colors.orange[800],
-                    child: Text("Take Another Image",
-                        style: TextStyle(fontFamily: fam)),
-                    onPressed: () {
-                      _uploaded = false;
-                      getImage(true);
-                    },
-                  ),
-
             // once the upload is complete, then  allow for
             // the user to download the image by calling the
             // downloadImage function which also returns the url
             // of the downloaded image
             _uploaded == false
-                ? Text("Press Upload to Firebase to find out more info!",
+                ? Text("Press the museum icon to find out more!",
                     style: TextStyle(fontFamily: fam))
                 : RaisedButton(
                     color: Colors.orange[800],
@@ -225,75 +287,18 @@ class _HPState extends State<Home> {
                       ).show();
                     },
                   ),
-            //Font Settings
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text("Settings",
-                  style: TextStyle(
-                      fontFamily: fam,
-                      fontSize: 30,
-                      decoration: TextDecoration.underline)),
-            ),
-
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: <
-                Widget>[
-              Text("Font: ", style: TextStyle(fontFamily: fam, fontSize: 20)),
-              ToggleButtons(
-                children: <Widget>[
-                  Text("A",
-                      style: TextStyle(fontFamily: 'Comic Neue', fontSize: 10)),
-                  Text("A",
-                      style: TextStyle(fontFamily: 'Montserrat', fontSize: 10)),
-                  Text("A",
-                      style: TextStyle(fontFamily: 'OpenSans', fontSize: 10)),
-                ],
-                onPressed: (int index) {
-                  setState(() {
-                    for (int buttonIndex = 0;
-                        buttonIndex < isSelected.length;
-                        buttonIndex++) {
-                      if (buttonIndex == index) {
-                        isSelected[buttonIndex] = true;
-                        fam = fonts[buttonIndex];
-                      } else {
-                        isSelected[buttonIndex] = false;
-                      }
-                    }
-                  });
-                },
-                isSelected: isSelected,
-              ),
-            ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Toggle Mode: ",
-                  style: TextStyle(fontFamily: fam, fontSize: 20),
-                ),
-                Switch(
-                  value: (_themeChanger.getTheme() == ThemeData.dark()),
-                  onChanged: (value) {
-                    setState(() {
-                      _themeChanger.getTheme() == ThemeData.dark()
-                          ? _themeChanger.setTheme(ThemeData.light())
-                          : _themeChanger.setTheme(ThemeData.dark());
-                    });
-                  },
-                  activeTrackColor: Colors.orange[500],
-                  activeColor: Colors.orange[800],
-                ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(30),
-              child: Image.asset(
-                'assets/images/TranspMuseum.png',
-                width: 100,
-                height: 100,
-                scale: 5,
-              ),
-            ),
+            // if the image is not selected don't show the option to upload to firebase storage
+            _imageFile == null
+                ? Container()
+                : RaisedButton(
+                    color: Colors.orange[800],
+                    child: Text("Take Another Image",
+                        style: TextStyle(fontFamily: fam)),
+                    onPressed: () {
+                      _uploaded = false;
+                      getImage(true);
+                    },
+                  ),
           ])),
         ));
   }
